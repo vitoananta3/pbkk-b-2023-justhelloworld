37869f6b2ce12984ffa2165cd4f72b5c82b57e54
Vito Febrian Ananta <vitoananta3@gmail.com>
2023-12-08 10:18:48 +0700

    Init

---
+++ app/Views/pages/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vito Ananta - Personal Website</title>
+</head>
+<body>
+    <h1>Welcome to Vito Ananta's Personal Website</h1>
+    <p>Hello, I'm Vito Ananta. This is my personal website where I share my thoughts, projects, and experiences.</p>
+    <p>Feel free to explore and get in touch with me!</p>
+</body>
+</html>
